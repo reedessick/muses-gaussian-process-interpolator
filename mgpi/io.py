299738379452c38d89d4e9d50edca7e5ff466738
Reed Essick <reed.essick@gmail.com>
2024-01-25 16:35:07 -0500

    added a few placeholder methods to I/O

--- mgpi/io.py
+++ mgpi/io.py
@@ -4,4 +4,21 @@ __author__ = "Reed Essick (reed.essick@gmail.com)"
 
 #-------------------------------------------------
 
+def load(*args, **kwargs):
+    """load tabular data from disk
+    """
+    raise NotImplementedError
 
+#------------------------
+
+def save(*args, **kwargs):
+    """write tabular data to disk
+    """
+    raise NotImplementedError
+
+#-------------------------------------------------
+
+def parse(*args, **kwargs):
+    """instantiate a kernel based on a config file
+    """
+    raise NotImplementedError
