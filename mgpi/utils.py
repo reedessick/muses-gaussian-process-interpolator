4e1a320e2cc5d3f32ca1305482218089625c7a8d
Reed Essick <reed.essick@gmail.com>
2024-01-27 17:56:31 -0500

    wrote a basic function to split the data into testing and training sets

--- mgpi/utils.py
+++ mgpi/utils.py
@@ -28,7 +28,25 @@ def _factory(klass):
 
 #-------------------------------------------------
 
-def subdivide_data(source_x, source_f, **kwargs):
-    """divide the data into subsets for testing and training
+def subdivide_data(source_x, source_f, frac=0.5, verbose=False):
+    """divide the data into subsets for testing and training. We randomly assign "frac" of the data \
+into the train data set and "1 - frac" into the test data set.
     """
-    raise NotImplementedError
+    # figure out the sizes of the subsets
+    num = len(source_x)
+    num_train = int(round(frac*num, 0))
+
+    if verbose:
+        print('dividing data set of %d samples into %d training samples and %d testing samples' % \
+            (num, num_train, num-num_train))
+
+    assert (num_train > 0), 'will not partion data into training set with zero samples'
+    assert (num > num_train), 'will not partion data into training set with zero samples'
+
+    # generate random subsamples
+    inds = np.arange(num)
+    np.random.shuffle(inds) # randomly shuffle order
+
+    # return the subsets
+    #       training data                                             test data
+    return (source_x[inds[:num_train]], source_f[inds[:num_train]]), (source_x[inds[num_train:]], source_f[inds[num_train:]])
